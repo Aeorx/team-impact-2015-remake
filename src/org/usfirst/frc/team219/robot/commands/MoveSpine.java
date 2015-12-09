9a6b7f87ceeeb3c0a987f84e798da05585351985
Aeorx <justinwestley11@yahoo.com>
2015-12-09 16:57:32 -0500

    modified the spine and move spine

--- src/org/usfirst/frc/team219/robot/commands/MoveSpine.java
+++ src/org/usfirst/frc/team219/robot/commands/MoveSpine.java
@@ -1,9 +1,16 @@
 package org.usfirst.frc.team219.robot.commands;
 
+import org.usfirst.frc.team219.robot.Robot;
+
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.command.Command;
 
 public class MoveSpine extends Command{
-
+	public MoveSpine()
+	{
+		requires(Robot.spine);
+	}
+	
 	@Override
 	protected void initialize() {
 		// TODO Auto-generated method stub
@@ -12,8 +19,7 @@ public class MoveSpine extends Command{
 
 	@Override
 	protected void execute() {
-		// TODO Auto-generated method stub
-		
+		Robot.spine.controlSpine(Robot.oi.stick.getY());
 	}
 
 	@Override
--- src/org/usfirst/frc/team219/robot/subsystems/Spine.java
+++ src/org/usfirst/frc/team219/robot/subsystems/Spine.java
@@ -1,14 +1,42 @@
 package org.usfirst.frc.team219.robot.subsystems;
 
+import org.usfirst.frc.team219.robot.RobotMap;
 import org.usfirst.frc.team219.robot.commands.*;
 
+import edu.wpi.first.wpilibj.CANTalon;
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 public class Spine extends Subsystem{
+	Encoder spineEncoder;
+	DigitalInput limitTop;
+	DigitalInput limitBot;
+	CANTalon spineTalon;
+	
+	public Spine()
+	{
+		spineTalon = RobotMap.spineTalon;
+		spineEncoder = RobotMap.spineEncoder;
+		limitTop = RobotMap.limitTop;
+		limitBot = RobotMap.limitBot;
 
+	}
 	@Override
 	protected void initDefaultCommand() {
 		setDefaultCommand(new MoveSpine());
+		
+	}
+	
+	public void controlSpine(double speed)
+	{
+		if(limitTop.get() && limitBot.get())
+			spineTalon.set(speed);
+	}
+	
+	public void stopSpine()
+	{
 		
 	}
 
